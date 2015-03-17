0345b3fff53b8335d03a11e4bb4355ae4a9d8883
jordan <jordan@millenniumgd.com>
2015-03-17 09:05:34 -0400

    Nightly Build 2015-03-16

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,21 @@ module.exports = [
         "clobbers": [
             "wkwebview"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.telerik.plugins.wkwebview": "0.3.5"
+    "com.telerik.plugins.wkwebview": "0.3.5",
+    "org.apache.cordova.inappbrowser": "0.6.1-dev"
 }
 // BOTTOM OF METADATA
 });
