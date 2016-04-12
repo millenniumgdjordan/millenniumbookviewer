881a2b0a5d99fbf6100838b00f5f700a4adeae98
Jordan <jordan@millenniumgd.com>
2016-04-12 13:24:15 -0400

    Resuming work

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -106,9 +106,6 @@ var app = {
        
         app.receivedEvent('deviceready');
         navigator.splashscreen.hide();
-        $( ":mobile-pagecontainer" ).on( "pagecontainershow", function( event, ui ) {
-          console.log( "A page was just loaded " + ui.toPage );
-        });
          // Resive video
     
     },
@@ -518,11 +515,15 @@ $( document ).on("pageshow", "#login", function() {
 });
 
 $(document).on('pagecontainershow', function(e, ui) {
+    console.log('pagecontainershowinit');
     var pageId = $('body').pagecontainer('getActivePage').prop('id');
+    console.log(pageId);
     scaleVideoContainer();
+    console.log('just scaled VideoContainer');
     initBannerVideoSize('.video-container .poster img');
     initBannerVideoSize('.video-container .filter');
     initBannerVideoSize('.video-container video');
+    console.log('inited video container elements');
 
     $(window).on('resize', function() {
         setTimeout( function() {
